9791fcc068617f2a687e9721e89b47a0f51ece32
clairemuller <clairermuller@gmail.com>
2019-05-20 21:49:56 -0700

    create custom route to fetch everything

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,28 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.find_or_create_by(name: params["user"])
-    render json: @user
+    user = User.find_or_create_by(name: params["username"])
+    render json: user
   end
 
   def show
-    @user = User.find(params[:id])
-    render json: @user
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  def update
+    byebug
+    # @item = Item.find_or_create_by(description: params['description'])
+  end
+
+  def getEverything
+    user = User.find(params[:id])
+    everything = {
+      locations: user.locations,
+      rooms: user.rooms,
+      categories: user.categories.uniq
+    }
+    render json: everything
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :rooms
   resources :properties
   resources :users
+  get '/users/:id/everything', to: 'users#getEverything'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
