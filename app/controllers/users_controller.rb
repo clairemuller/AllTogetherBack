8938cc3d8b6efd43125c08576427ca1305aeb944
clairemuller <clairermuller@gmail.com>
2019-05-21 15:27:45 -0700

    add routes, update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,18 +16,34 @@ class UsersController < ApplicationController
   end
 
   def update
-    byebug
     # @item = Item.find_or_create_by(description: params['description'])
   end
 
   def getEverything
     user = User.find(params[:id])
     everything = {
-      locations: user.locations,
       rooms: user.rooms,
+      locations: user.locations,
       categories: user.categories.uniq
     }
     render json: everything
+  end
+
+  def getItems
+    user = User.find(params[:id])
+    render json: user.items
+  end
+
+  def addItem
+    user = User.find(params[:id])
+    byebug
+    # Item.create(description: params['description'], location: params['location'])
+  end
+
+  def addRoom
+    user = User.find(params[:id])
+    room = Room.find_or_create_by(name: params["room"], property_id: 1)
+    render json: room
   end
 
 end
